e54d1e6959565beddef8994392377b3d4993015b
AjayGupta-8828 <ajaysonugupta5698@gmail.com>
2026-04-06 23:01:32 +0530

    added render host to allowed host

--- Dproject/settings.py
+++ Dproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['taskflow-1-6dae.onrender.com']
 AUTH_USER_MODEL='accounts.CustomUser'
 
 
